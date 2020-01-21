4a65ab6a4d6f4b57d89de70812bb9494ba068211
elenaveltroni <43828256+elenaveltroni@users.noreply.github.com>
2020-01-21 18:27:45 +0100

    Cambiato risultato query

--- src/task2/ResultController.java
+++ src/task2/ResultController.java
@@ -4,19 +4,20 @@ import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javafx.fxml.Initializable;
 
 public class ResultController implements Initializable {
 	
-	private JSONObject TotalProduction = new JSONObject();
-	private JSONObject AvgProduction = new JSONObject();
-//	private JSONObject top5Production = null;
-	private JSONObject TotalImport = new JSONObject();
-	private JSONObject AvgImport = new JSONObject();
-	private JSONObject TotalExport = new JSONObject();
-	private JSONObject AvgExport = new JSONObject();
+	private JSONArray TotalProduction = new JSONArray();
+	private JSONArray AvgProduction = new JSONArray();
+	private JSONArray top5Production =new JSONArray();
+	private JSONArray TotalImport = new JSONArray();
+	private JSONArray AvgImport = new JSONArray();
+	private JSONArray TotalExport = new JSONArray();
+	private JSONArray AvgExport = new JSONArray();
 
 	public void initResult(String food, String region, String country, String aim, String start, String end, String aggregation) {
 		if(aim.equals("Production")) {
@@ -29,36 +30,40 @@ public class ResultController implements Initializable {
 				System.out.println(AvgProduction);
 			}
 			else if(aggregation.contentEquals("Top 5")) {
-				AvgProduction = MongoHandler.getTop5Production(food,region,start,end);
-				System.out.println(AvgProduction);
+				top5Production = MongoHandler.getTop5Production(food,region,start,end);
+				System.out.println(top5Production);
 			}
 		}
 		else if(aim.equals("Import")) {
 			if(aggregation.equals("Sum")) {
-				TotalImport = MongoHandler.getTotalImport(food,region,country,start,end);
+				if(region != null)
+					TotalImport = MongoHandler.getTotalRegionImport(food,region,start,end);
+				else
+					TotalImport = MongoHandler.getTotalCountryImport(food,country,start,end);
 				System.out.println(TotalImport);
 			}
 			else if(aggregation.contentEquals("Average")) {
-				AvgImport = MongoHandler.getAverageImport(food,region,country,start,end);
+				if(region != null)
+					AvgImport = MongoHandler.getAverageRegionImport(food,region,start,end);
+				else
+					AvgImport = MongoHandler.getTotalCountryImport(food,country,start,end);
 				System.out.println(AvgImport);
-			}
-			else if(aggregation.contentEquals("Top 5")) {
-				AvgProduction = MongoHandler.getAverageProduction(food,region,country,start,end);
-				System.out.println(AvgProduction);
 			}
 		}
 		else if(aim.equals("Export")) {
 			if(aggregation.equals("Sum")) {
-				TotalExport = MongoHandler.getTotalExport(food,region,country,start,end);
+				if(region != null)
+					TotalExport = MongoHandler.getTotalRegionExport(food,region,start,end);
+				else
+					TotalExport = MongoHandler.getTotalCountryExport(food,country,start,end);
 				System.out.println(TotalExport);
 			}
 			else if(aggregation.contentEquals("Average")) {
-				AvgExport = MongoHandler.getAverageExport(food,region,country,start,end);
+				if(region != null)
+					TotalExport = MongoHandler.getAverageRegionExport(food,region,start,end);
+				else
+					TotalExport = MongoHandler.getTotalCountryExport(food,country,start,end);
 				System.out.println(AvgExport);
-			}
-			else if(aggregation.contentEquals("Top 5")) {
-				AvgProduction = MongoHandler.getAverageProduction(food,region,country,start,end);
-				System.out.println(AvgProduction);
 			}
 		}
 	}
