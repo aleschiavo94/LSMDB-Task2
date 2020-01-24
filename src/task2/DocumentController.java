60ee989277c0b59a0b53bb59630005e60683f140
Federica <Federica@federica>
2020-01-24 19:07:03 +0100

    .

--- src/task2/DocumentController.java
+++ src/task2/DocumentController.java
@@ -150,7 +150,7 @@ public class DocumentController implements Initializable {
 	        dialogStage.show();
 		}
         		
-        //ResultController controller = loader.getController();
+        
               
          
 	}
